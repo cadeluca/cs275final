8a47abc0951f34bf7d2558d812993f3a764a6f01
Tan-Junda <jtan5@uvm.edu>
2019-10-03 02:22:43 +0800

    CrimeListActivity -> PriceListActivity && CrimeListFragment -> PriceListFragmen

--- app/src/main/java/cs275/gaspricetracker/PriceListFragment.java
+++ app/src/main/java/cs275/gaspricetracker/PriceListFragment.java
@@ -21,12 +21,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 
-public class CrimeListFragment extends Fragment {
+public class PriceListFragment extends Fragment {
 
     private static final String SAVED_SUBTITLE_VISIBLE = "subtitle";
 
-    private RecyclerView mCrimeRecyclerView;
-    private CrimeAdapter mAdapter;
+    private RecyclerView mPriceRecyclerView;
+    private PriceAdapter mAdapter;
     private boolean mSubtitleVisible;
 
     @Override
@@ -38,11 +38,11 @@ public class CrimeListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_Price_list, container, false);
 
-        mCrimeRecyclerView = (RecyclerView) view
-                .findViewById(R.id.crime_recycler_view);
-        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mPriceRecyclerView = (RecyclerView) view
+                .findViewById(R.id.Price_recycler_view);
+        mPriceRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         if (savedInstanceState != null) {
             mSubtitleVisible = savedInstanceState.getBoolean(SAVED_SUBTITLE_VISIBLE);
@@ -68,7 +68,7 @@ public class CrimeListFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.fragment_crime_list, menu);
+        inflater.inflate(R.menu.fragment_Price_list, menu);
 
         MenuItem subtitleItem = menu.findItem(R.id.show_subtitle);
         if (mSubtitleVisible) {
@@ -81,11 +81,11 @@ public class CrimeListFragment extends Fragment {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.new_crime:
-                Crime crime = new Crime();
-                CrimeLab.get(getActivity()).addCrime(crime);
-                Intent intent = CrimePagerActivity
-                        .newIntent(getActivity(), crime.getId());
+            case R.id.new_Price:
+                Price Price = new Price();
+                PriceLab.get(getActivity()).addPrice(Price);
+                Intent intent = PricePagerActivity
+                        .newIntent(getActivity(), Price.getId());
                 startActivity(intent);
                 return true;
             case R.id.show_subtitle:
@@ -99,9 +99,9 @@ public class CrimeListFragment extends Fragment {
     }
 
     private void updateSubtitle() {
-        CrimeLab crimeLab = CrimeLab.get(getActivity());
-        int crimeCount = crimeLab.getCrimes().size();
-        String subtitle = getString(R.string.subtitle_format, crimeCount);
+        PriceLab PriceLab = PriceLab.get(getActivity());
+        int PriceCount = PriceLab.getPrices().size();
+        String subtitle = getString(R.string.subtitle_format, PriceCount);
 
         if (!mSubtitleVisible) {
             subtitle = null;
@@ -112,79 +112,79 @@ public class CrimeListFragment extends Fragment {
     }
 
     private void updateUI() {
-        CrimeLab crimeLab = CrimeLab.get(getActivity());
-        List<Crime> crimes = crimeLab.getCrimes();
+        PriceLab PriceLab = PriceLab.get(getActivity());
+        List<Price> Prices = PriceLab.getPrices();
 
         if (mAdapter == null) {
-            mAdapter = new CrimeAdapter(crimes);
-            mCrimeRecyclerView.setAdapter(mAdapter);
+            mAdapter = new PriceAdapter(Prices);
+            mPriceRecyclerView.setAdapter(mAdapter);
         } else {
-            mAdapter.setCrimes(crimes);
+            mAdapter.setPrices(Prices);
             mAdapter.notifyDataSetChanged();
         }
 
         updateSubtitle();
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder
+    private class PriceHolder extends RecyclerView.ViewHolder
             implements View.OnClickListener {
 
-        private Crime mCrime;
+        private Price mPrice;
 
         private TextView mTitleTextView;
         private TextView mDateTextView;
         private ImageView mSolvedImageView;
 
-        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
-            super(inflater.inflate(R.layout.list_item_crime, parent, false));
+        public PriceHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_Price, parent, false));
             itemView.setOnClickListener(this);
 
-            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
-            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
-            mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
+            mTitleTextView = (TextView) itemView.findViewById(R.id.Price_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.Price_date);
+            mSolvedImageView = (ImageView) itemView.findViewById(R.id.Price_solved);
         }
 
-        public void bind(Crime crime) {
-            mCrime = crime;
-            mTitleTextView.setText(mCrime.getTitle());
-            mDateTextView.setText(mCrime.getDate().toString());
-            mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
+        public void bind(Price Price) {
+            mPrice = Price;
+            mTitleTextView.setText(mPrice.getTitle());
+            mDateTextView.setText(mPrice.getDate().toString());
+            mSolvedImageView.setVisibility(Price.isSolved() ? View.VISIBLE : View.GONE);
         }
 
         @Override
         public void onClick(View view) {
-            Intent intent = CrimePagerActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = PricePagerActivity.newIntent(getActivity(), mPrice.getId());
             startActivity(intent);
         }
     }
 
-    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+    private class PriceAdapter extends RecyclerView.Adapter<PriceHolder> {
 
-        private List<Crime> mCrimes;
+        private List<Price> mPrices;
 
-        public CrimeAdapter(List<Crime> crimes) {
-            mCrimes = crimes;
+        public PriceAdapter(List<Price> Prices) {
+            mPrices = Prices;
         }
 
         @Override
-        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public PriceHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
-            return new CrimeHolder(layoutInflater, parent);
+            return new PriceHolder(layoutInflater, parent);
         }
 
         @Override
-        public void onBindViewHolder(CrimeHolder holder, int position) {
-            Crime crime = mCrimes.get(position);
-            holder.bind(crime);
+        public void onBindViewHolder(PriceHolder holder, int position) {
+            Price Price = mPrices.get(position);
+            holder.bind(Price);
         }
 
         @Override
         public int getItemCount() {
-            return mCrimes.size();
+            return mPrices.size();
         }
 
-        public void setCrimes(List<Crime> crimes) {
-            mCrimes = crimes;
+        public void setPrices(List<Price> Prices) {
+            mPrices = Prices;
         }
     }
 }
