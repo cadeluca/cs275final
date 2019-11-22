19380d8967bd3e2a239a3dfc01a743c4d29cde26
Tan-Junda <jtan5@uvm.edu>
2019-11-22 11:36:12 +0800

    MainActivity backarrow destroy app

--- app/src/main/java/cs275/gaspricetracker/MainActivity.java
+++ app/src/main/java/cs275/gaspricetracker/MainActivity.java
@@ -1,5 +1,8 @@
 package cs275.gaspricetracker;
 
+import android.content.Intent;
+import android.view.KeyEvent;
+
 import androidx.fragment.app.Fragment;
 
 public class MainActivity extends SingleFragmentActivity {
@@ -8,7 +11,16 @@ public class MainActivity extends SingleFragmentActivity {
     protected Fragment createFragment() {
         return new MainFragment();
     }
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK){
+            onDestroy();
+            return true;
+        } else {
+            return super.dispatchKeyEvent(event);
+        }
 
+    }
 }
 
 
